b3e38d0fe63e5dfd461c5ccbe28f1a03eb11ed61
shahjad-k <147805873+shahjad-k@users.noreply.github.com>
2025-01-03 16:29:22 +0530

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,13 @@
+# # print numbers from 1 to 5
+# i=1
+# while i<=5:
+#     print(i)
+#     i+=1
+#     print("loop ended")
+
+#     # don't apply infinite loop 
+# i=5
+# while i<6:
+#     print(i)
+#     i-=1
+#     print("loop ended")
